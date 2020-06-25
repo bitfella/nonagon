b07700feccd697ad6cab7eb8d22cbae052ad130d
bitfella <hello@bitfella.net>
2020-06-25 20:21:25 +0200

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { Provider } from 'react-redux';
 import React from 'react';
 import { render } from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from 'components/App';
 import configureStore, { history } from 'configureStore';
+
+Sentry.init({ dsn: "https://004eeaed176f483abd20cb735c00f174@o412640.ingest.sentry.io/5290599" });
 
 const store = configureStore();
 
