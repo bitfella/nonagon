2f6a1dad93fe4fd8a48e23493b3f02511de68de9
bitfella <hello@bitfella.net>
2020-06-30 15:10:01 +0200

    Fix app url in README.md

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from 'components/App';
 import configureStore, { history } from 'configureStore';
 
 Sentry.init({
-  dsn: "https://004eeaed176f483abd20cb735c00f174@o412640.ingest.sentry.io/5290599",
+  dsn: process.env.REACT_APP_SENTRY_DSN,
   environment: process.env.NODE_ENV
 });
 
